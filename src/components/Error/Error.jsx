6c57a6c0a90624cde3953afe1e0f4e86425a5487
Nicolás Salvo <nicosalvo@gmail.com>
2023-05-04 17:02:34 -0300

    refactor: change error message

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,6 +1,9 @@
 const Error = () => {
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>404, page not found 🥲</div>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', flexDirection: 'column'}}>
+      <span style={{margin: '5px' }}>😿 404, page not found 🥲</span>
+      <img src="https://http.cat/404" alt="404 cat error" width='300px'/>
+    </div>
   )
 }
 

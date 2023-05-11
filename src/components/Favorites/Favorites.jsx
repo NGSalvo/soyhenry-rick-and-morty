7bf72ad2109bdf2ba25e284293733e4a57fe493f
Nicolás Salvo <nicosalvo@gmail.com>
2023-05-11 17:53:45 -0300

    feat: add favorites feat: order favorites feat: filter favorites

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,24 +1,53 @@
-import { useSelector } from "react-redux"
 import Card from "../Card/Card";
 import style from './Favorites.module.css'
+import { useSelector, useDispatch } from "react-redux"
+import { filterCards, orderCards  } from "../../redux/actions";
 
 export const Favorites = () => {
   const characters = useSelector(state => state.myFavorites)
+  const dispatch = useDispatch()
+
+  const handleOrder = (event) => {
+    dispatch(orderCards(event.target.value))
+  }
+  
+  const handleFilter = (event) => {
+    dispatch(filterCards(event.target.value))
+
+  }
+
   return (
-    <div className={style.container}>{
-      characters.map((character) => 
-        <Card
-          key={character.id}
-          id={character.id}
-          name={character.name}
-          status={character.status}
-          species={character.species}
-          gender={character.gender}
-          origin={character.origin.name}
-          image={character.image}
-          onClose={character.onClose}
-        />
-        )
-      }
-    </div>)
+    <div>
+      <select onChange={handleOrder}>
+        <option value="A">Ascendente</option>
+        <option value="D">Descendente</option>
+      </select>
+      <select onChange={handleFilter}>
+        <option value="Everyone">Todos</option>
+        <option value="Male">Hombre</option>
+        <option value="Female">Mujer</option>
+        <option value="Genderless">Sin género</option>
+        <option value="unknown">Desconocido</option>
+      </select>
+      <div className={style.container}>
+        {
+          characters.map((character) => 
+            <Card
+              key={character.id}
+              id={character.id}
+              name={character.name}
+              status={character.status}
+              species={character.species}
+              gender={character.gender}
+              origin={character.origin}
+              image={character.image}
+              onClose={character.onClose}
+            />
+            )
+        }
+      </div>
+    </div>
+    )
 }
+
+// TODO: Si revisas, esta aplicación tiene un pequeño bug que tendrás que resolver... cuando presionas el ❤️ de una de las Cards el personaje aparece en la vista de "Favoritos". Pero si luego eliminas el personaje precionando en la X, este aún permanece en esa vista. Busca la manera para que cuando elimines un personaje, también se elimine de "Favoritos".
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-types"
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./action-types"
 
 const initialState = {
-  myFavorites: []
+  myFavorites: [],
+  allCharacters: []
 }
 
 export const reducer = (state = initialState, action) => {
@@ -9,14 +10,33 @@ export const reducer = (state = initialState, action) => {
     case ADD_FAV:
       return {
         ...state,
-        myFavorites: [...state.myFavorites, action.payload]
+        myFavorites: [...state.allCharacters, action.payload],
+        allCharacters: [...state.allCharacters, action.payload]
       }
+    
     case REMOVE_FAV:
       const filteredFavorites = state.myFavorites.filter(char => char.id !== action.payload)
       return {
         ...state,
         myFavorites: filteredFavorites
       }
+    case FILTER:
+      let filteredCharacters = state.allCharacters.filter(character => character.gender === action.payload)
+      if (action.payload === 'Everyone') {
+        filteredCharacters = [...state.allCharacters]
+      }
+      return {
+        ...state,
+        myFavorites: filteredCharacters
+      }
+
+    case ORDER:
+      const copyOfFavoriteCharacters = [...state.allCharacters]
+      return {
+        ...state,
+        myFavorites: action.payload === 'A' ? copyOfFavoriteCharacters.sort((a,b) => a.id - b.id) : copyOfFavoriteCharacters.sort((a,b) => b.id - a.id)
+      }
+
     default:
       return {...state}
   }
